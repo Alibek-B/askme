bfcb8879cbd768682d197f55399b0817498c65c4
alibek <bajmuhametovalibek30@gmail.com>
2021-04-15 14:21:04 +0300

    Views refoctoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1, 
+        name: 'Alibek',
+        username: 'ibb',
+      ),
+
+      User.new(
+        id: 2, 
+        name: 'Al', 
+        username: 'al',
+      )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Hello, my friend!"
+    @user = User.new(
+      name: 'Alibek',
+      username: 'ibb',
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('14.04.2021')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('14.04.2021')),
+    ]
+
+    @new_question = Question.new
   end
 end
